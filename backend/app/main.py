64e845df10b28c13705a89a11f2fa8579ac244af
yousraKass <kassousyousra@gmail.com>
2026-09-19 22:31:25 +0100

    initialize routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from .routers import papers
+from .routers import papers, research
 
 app = FastAPI()
 
 app.include_router(papers.router)
+app.include_router(research.router)
 
 
 @app.get('/health')
